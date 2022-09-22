f8379852e604bd83ba6097cafa5c534c6817d4ca
Ki-Linux <89332130+Ki-Linux@users.noreply.github.com>
2022-09-22 15:03:46 +0900

    add #

--- components/back_button/back.vue
+++ components/back_button/back.vue
@@ -8,32 +8,33 @@ import { Vue,Component, Prop } from 'vue-property-decorator';
 @Component
 export default class backButton extends Vue {
     word_button: string = "←トップ画面に戻る";
+
     @Prop()
     public where_go!: string;
+
     created() {
         if(this.where_go === "account") { //ホーム画面に戻る
             this.word_button = "←戻る";
         }
     }
+
     goBack() {
         let send_url = '/';
         if(this.where_go === "account") {
-            
             //アカウント画面に戻る
             const name = this.$store.state.username;
-            send_url = '/myaccount/mypage/myaccountpage?myname=' + name;
-            
+            send_url = '/myaccount/mypage/myaccountpage?myname=' + name;         
         }
         this.$router.push(send_url);
     }
 }
 </script>
 <style lang="scss">
-#back {
-    font-size: 30px;
-    button {
-        padding: 3px 10px;
-        background-color: rgb(241, 241, 241);
+    #back {
+        font-size: 30px;
+        button {
+            padding: 3px 10px;
+            background-color: rgb(241, 241, 241);
+        }
     }
-}
 </style>
