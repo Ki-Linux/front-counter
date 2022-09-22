8e8f23792d9b4677e5924203696034cf39fd668c
Ki-Linux <89332130+Ki-Linux@users.noreply.github.com>
2022-09-22 15:09:15 +0900

    rm space

--- components/mypage/my_list.vue
+++ components/mypage/my_list.vue
@@ -10,7 +10,7 @@
                 <li v-if="picture_data.picture === post_url+'notImg'"></li>
                 <li v-else><img :src="picture_data.picture" alt="写真"></li>
                 <li>{{ picture_data.my_comment }}</li>
-                <li>{{ picture_data.created_at }}</li>
+                <li><span>投稿日 </span>{{ picture_data.created_at }}</li>
             </ul>  
         </div>
     </div>
@@ -19,12 +19,21 @@
 import { Vue, Component, Prop } from 'vue-property-decorator';
 @Component
 export default class myList extends Vue {
-    picturesData: { id: number; my_comment: string; picture: string|number|ArrayBuffer; can_see: number; created_at: string }[] = [];
+    picturesData: { 
+        id: number,
+        my_comment: string,
+        picture: string|number|ArrayBuffer,
+        can_see: number,
+        created_at: string 
+    }[] = [];
     post_url: string | undefined;
+
     @Prop()
     public list_username!: string;
+
     mounted() {//データを表示する
         this.post_url = process.env.SERVER_URL; 
+
         this.$axios.get("edit_show", {
             params: {
                 username: this.list_username,
@@ -40,105 +49,115 @@ export default class myList extends Vue {
                 const url = process.env.SERVER_URL;
                 data[i].picture = url + data[i].picture;
                 this.picturesData.splice(i, 1, data[i]);
-            }
-            
+            }       
         })
         .catch((err) => {
             console.log(err);
-        });
-    
+        });   
     }
+
     detailShow(show_num: number) {
-        let send_data: { id: number; picture: string|number|ArrayBuffer; my_comment: string; username: string;  updated_at: string; can_see: number; };
+        let send_data: { 
+            id: number, 
+            picture: string|number|ArrayBuffer, 
+            my_comment: string, 
+            username: string,  
+            updated_at: string, 
+            can_see: number
+        };
         const content = this.picturesData[show_num];
         const send_id = content.id;
         const send_picture = content.picture;
         const send_comment = content.my_comment;
         const send_day = content.created_at;
         const send_see = content.can_see;
-        send_data = { id: send_id, picture: send_picture, my_comment: send_comment, username: this.list_username, updated_at: send_day, can_see: send_see};
+        send_data = { 
+            id: send_id, 
+            picture: send_picture, 
+            my_comment: send_comment, 
+            username: this.list_username, 
+            updated_at: send_day, 
+            can_see: send_see
+        };
         this.$emit('detail_data_show', [true, send_data]);
     }
 }
 </script>
 <style lang="scss">
-@mixin sp {
-    @media (max-width: 560px) {
-        @content;
-    }
-}
-#my_list {
-    width: 900px;
-    background-color: bisque;
-    margin: 20px auto;
-    text-align: center;
-    padding-bottom: 50px;
-    @include sp {
-        width: 100%;
-    }
-    .toeveryone {
-        position: absolute;
-        padding: 20px;
-        .everyone_page {
-            text-decoration: none;
-            font-size: 20px;
+    @mixin sp {
+        @media (max-width: 560px) {
+            @content;
         }
     }
-}
-.list {
-    padding-top: 60px;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-            
-    ul {
-        width: 350px;
-        background-color: rgba(255, 255, 255, 0.6);
-        padding-bottom: 30px;
-        border: 1px solid red;
-        margin-left: 20px;
-        list-style: none;
-        @include sp {
-            width: 130px;
-        }         
-                
-        li {
-            width: 80%;
-            margin-left: 15px;
-            @include sp {
-                width: 100%;
-                margin-left: -20px;
-            }  
-            
-            &:first-of-type {
-                margin-top: 30px;
-                  
-                img {
-                    width: 50%;
-                    max-height: 150px;
-                    background-color: rgba(0, 0, 0, 0.1);
-                    padding: 5px;
-                    transform: translateX(50%);
-                    @include sp {
-                        width: 80px;
-                        transform: translateX(0%);
-                    }  
-                }
-                
-            }
-            &:nth-of-type(2) {
-                
-                margin-top: 30px;
-                padding: 10px;
-                background-color: white;
-                word-break: break-all;
-                @include sp {
-                    font-size: 10px;
 
-                } 
+    #my_list {
+        width: 900px;
+        background-color: bisque;
+        margin: 20px auto;
+        text-align: center;
+        padding-bottom: 50px;
+        @include sp {
+            width: 100%;
+        }
+
+        .toeveryone {
+            position: absolute;
+            padding: 20px;
+            .everyone_page {
+                text-decoration: none;
+                font-size: 20px;
             }
         }
     }
+
+    .list {
+        padding-top: 60px;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;       
+        ul {
+            width: 350px;
+            background-color: rgba(255, 255, 255, 0.6);
+            padding-bottom: 30px;
+            border: 1px solid red;
+            margin-left: 20px;
+            list-style: none;
+            @include sp {
+                width: 130px;
+            }         
                 
-}
+            li {
+                width: 80%;
+                margin-left: 15px;
+                @include sp {
+                    width: 100%;
+                    margin-left: -20px;
+                }  
+            
+                &:first-of-type {
+                    margin-top: 30px;           
+                    img {
+                        width: 50%;
+                        max-height: 150px;
+                        background-color: rgba(0, 0, 0, 0.1);
+                        padding: 5px;
+                        transform: translateX(50%);
+                        @include sp {
+                            width: 80px;
+                            transform: translateX(0%);
+                        }  
+                    }           
+                }
+                &:nth-of-type(2) {   
+                    margin-top: 30px;
+                    padding: 10px;
+                    background-color: white;
+                    word-break: break-all;
+                    @include sp {
+                        font-size: 10px;
+                    } 
+                }
+            }
+        }                 
+    }
 </style>
